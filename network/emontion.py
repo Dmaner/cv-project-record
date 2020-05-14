ff6b039e36367641809617ed2e987d7b75066a28
dmaner <2663515256@qq.com>
2020-05-14 20:17:32 +0800

    finish sine and emotion

--- network/emontion.py
+++ network/emontion.py
@@ -21,21 +21,6 @@ class MyData(Dataset):
         return len(self.data)
 
 
-class Rnn(nn.Module):
-    def __init__(self, in_dim, hidden_dim, n_layer, n_classes):
-        super(Rnn, self).__init__()
-        self.n_layer = n_layer
-        self.hidden_dim = hidden_dim
-        self.lstm = nn.LSTM(in_dim, hidden_dim, n_layer, batch_first=True)
-        self.classifier = nn.Linear(hidden_dim, n_classes)
-
-    def forward(self, x):
-        out, (h_n, c_n) = self.lstm(x)
-        x = h_n[-1, :, :]
-        x = self.classifier(x)
-        return x
-
-
 class MyLSTM(nn.Module):
     def __init__(self, feature_size, hidden_size, output_size=1):
         super(MyLSTM, self).__init__()
